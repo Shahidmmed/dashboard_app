0dadc7556c431fab1a309f0bfbe8e8c114b506b5
Abdul-Shahid Mohammed <shahidmmed@gmail.com>
2022-09-03 18:49:51 +0000

    components and pages updates

--- src/App.js
+++ src/App.js
@@ -44,15 +44,15 @@ const App = () => {
     }
   }, []);
   return (
-    <div className="App">
+    <div className={currentMode === "Dark" ? "dark" : ""}>
       <BrowserRouter>
         <div className="flex relative dark:bg-main-dark-bg">
           <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
             <TooltipComponent content="Settings" position="Top">
               <button
                 type="button"
-                onClick={() => {}}
-                style={{ background: "blue", borderRadius: "50%" }}
+                onClick={() => setThemeSettings(true)}
+                style={{ background: currentColor, borderRadius: "50%" }}
                 className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray"
               >
                 <FiSettings />
